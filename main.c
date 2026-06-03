ca28d18c977f63ff4364d27fb4dca08acef06e15
Adrian <cvetkoadrian@gmail.com>
2026-06-03 23:04:24 +0200

    added a simple demo program coppied from guide

--- main.c
+++ main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hashTable.h"
 
-int main() {
+void exit_nomem(void) {
+    fprintf(stderr, "out of memory\n");
+    exit(1);
+}
 
-   return 0;
+int main(void) {
+    ht* counts = htCreate();
+    if (counts == NULL) {
+        exit_nomem();
+    }
+
+    // Read next word from stdin (at most 100 chars long).
+    char word[101];
+    while (scanf("%100s", word) != EOF) {
+        // Look up word.
+        void* value = htGet(counts, word);
+        if (value != NULL) {
+            // Already exists, increment int that value points to.
+            int* pcount = (int*)value;
+            (*pcount)++;
+            continue;
+        }
+
+        // Word not found, allocate space for new int and set to 1.
+        int* pcount = malloc(sizeof(int));
+        if (pcount == NULL) {
+            exit_nomem();
+        }
+        *pcount = 1;
+        if (htSet(counts, word, pcount) == NULL) {
+            exit_nomem();
+        }
+    }
+
+    // Print out words and frequencies, freeing values as we go.
+    hti it = htIterator(counts);
+    while (htNext(&it)) {
+        printf("%s %d\n", it.key, *(int*)it.value);
+        free(it.value);
+    }
+
+    // Show the number of unique words.
+    printf("%d\n", (int)htLength(counts));
+
+    htDestroy(counts);
+    return 0;
 }
