dd87f2ba6041c77ef8facd5a46c63ff19e2d177d
Adrian <cvetkoadrian@gmail.com>
2026-06-04 15:29:38 +0200

    added support functions

--- hashTable.h
+++ hashTable.h
@@ -2,12 +2,21 @@
 #ifndef _HASHTABLE_H
 #define _HASHTABLE_H
 
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
 
 #define INITIAL_CAPACITY 16
+
+// these can be modified later
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
 
 ///////////////////////////
 /// STRUCTURE DEFINITIONS
@@ -38,7 +47,7 @@ typedef struct hti{
 
 
 ///////////////////////////
-/// FUNCTION DECLARATIONS
+/// MAIN FUNCTION DECLARATIONS
 ///////////////////////////
 
 // creates a hash table and returns the pointer to it
@@ -65,9 +74,21 @@ hti htIterator(ht* table);
 bool htNext(hti* iterator);
 
 
+///////////////////////////
+/// SUPPORT FUNCTIONS DECLARATIONS
+///////////////////////////
+
+static uint64_t hashKey (const char* key);
+
+static const char* htSerEntry(item* entries, size_t capacity,
+        const char* key, void* value, size_t* plength);
+
+static bool htExpand(ht* table);
+
+
 
 ///////////////////////////
-/// FUNCTION DEFINITIONS
+/// MAIN FUNCTION DEFINITIONS
 ///////////////////////////
 
 ht* htCreate(void) {
@@ -91,11 +112,88 @@ ht* htCreate(void) {
     return table;
 }
 
+
 void htDestroy(ht *table) {
+    // right now, the value assigned to each key can be any type, as the type for the value is void*
+    // that means that the user has to manage their own allocated memory for the values.
+
+    // TODO:
+    // i am making it like this just for now in the beginning.
+    // i'll rework it later and also add a custom destructor for the allocated memory for the values
 
     for (size_t i = 0; i < table->capacity; i++) {
         free((void*)table->items[i].key);
     }
+
+    free(table->items);
+    free(table);
+}
+
+void* htGet(ht* table, const char* key) {
+    uint64_t hash = hashKey(key);
+
+    // AND hash with capacity-1 to ensure it's within entries array
+    size_t index = (size_t) (hash & (uint64_t)(table->capacity -1));
+
+    // during insertion, we always place a key in the first empty slot after its hashed position
+    // if we find NULL, that means the key way never inserted. that's why we can use the "!= NULL" condition
+    while (table->items[index].key != NULL) {
+        if (strcmp(key, table->items[index].key)) {
+
+            return table->items[index].value;
+        }
+
+        index++;
+
+        if (index >= table->capacity) {
+            // wrapping around if we reach the end of entries
+            index = 0;
+        }
+
+    }
+
+    return NULL;
+
+}
+
+
+
+const char* htSet(ht *table, const char *key, void *value) {
+     assert(value != NULL);
+
+     if (value == NULL) {
+         return NULL;
+     }
+
+     if (table->length >= (table->capacity / 2)) {
+         if (!htExpand(table)) {
+             return NULL;
+         }
+     }
+
+     return htSerEntry(table->items, table->capacity, key, value, &table->length);
+
+}
+
+
+///////////////////////////
+/// SUPPORT FUNCTIONS DEFINITIONS
+///////////////////////////
+
+static uint64_t hashKey (const char* key) {
+    uint64_t hash = FNV_OFFSET;
+
+    for (const char* p = key; *p != '\0'; p++) {
+
+        // (unsigned char) - ensures no negative values
+        // (uint64_t)- widens to 64 bits for XOR
+        hash = hash ^ (uint64_t)(unsigned char) (*p);
+
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+
 }
 
 
